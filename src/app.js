4f4dddf46e54f31b925cd306e7b34c61a66f1758
SriyansuDash <sriyansudash09@gmail.com>
2026-09-26 16:11:47 +0530

    cookie -> cookieParser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
-import authRoute from './routes/auth.routes';
-import cookie from 'cookie-parser'
+import authRoute from './routes/auth.routes.js';
+import cookieParser from 'cookie-parser'
 
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(cookieParser());
+
 app.use("/api/auth", authRoute);
 
 export default app
