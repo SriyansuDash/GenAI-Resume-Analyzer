64310ab15450ee0f43d1e80254f5fe2a3cf1fba8
SriyansuDash <sriyansudash09@gmail.com>
2026-09-26 16:10:55 +0530

    Authentication Router for login

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 import {Router} from 'express';
+import * as authController from '../controllers/auth.controller.js'
 
 const authRoute = Router();
 
+authRoute.post('/register', authController.register);
 
+authRoute.get('/login',authController.login);
 
 export default authRoute
